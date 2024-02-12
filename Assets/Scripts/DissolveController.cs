d3cc77a8dc109e4844bc028abf9c08544b6b0e1a
Basilísco <senstur.art@gmail.com>
2024-02-12 18:43:42 -0300

    0.4

--- Assets/Scripts/DissolveController.cs
+++ Assets/Scripts/DissolveController.cs
@@ -1,22 +1,24 @@
-using Unity.Mathematics;
 using UnityEngine;
 
 public class DissolveController : MonoBehaviour{
 
-    // [Range(0,1)]
-    // [SerializeField] private float _cutOff = 0.5f;
-
     [SerializeField] private Renderer _renderer;
-    [SerializeField] private float _dissolveSpeed = 3f;
+    [SerializeField] private float _dissolveSpeed = 1f;
+
+    [SerializeField] private GameObject _effect;
     private float _dissolveAmount = 1;
     private bool _dissolving;
 
     private void Update() {
-        // _renderer.material.SetFloat("_Cutoff", _cutOff);
+
         if(Input.GetKeyDown(KeyCode.D)){
             _dissolving = !_dissolving;
-        }
 
+            if(_effect != null){
+                _effect.SetActive(true);
+            }
+        }
+    
         if(_dissolving){
             _dissolveAmount = Mathf.MoveTowards(_dissolveAmount, 0f, _dissolveSpeed * Time.deltaTime);
         }else{
@@ -24,5 +26,11 @@ public class DissolveController : MonoBehaviour{
         }
 
         _renderer.material.SetFloat("_Cutoff", _dissolveAmount);
+
+        if(_dissolveAmount <= 0.5f){
+            _renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        }else{
+            _renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
     }
 }
